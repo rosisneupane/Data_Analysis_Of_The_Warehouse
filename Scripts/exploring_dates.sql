482b07f4c0f64a76331dac6a594d75362e769b9e
Rosis Neupane <46392658+rosisneupane@users.noreply.github.com>
2025-06-18 12:30:26 +1000

    Create exploring_dates.sql

---
+++ Scripts/exploring_dates.sql
@@ -0,0 +1,26 @@
+/*
+===============================================================================
+Date Range Exploration
+===============================================================================
+Summary:
+    Analyze key date boundaries and age distribution from fact and dimension data.
+
+SQL Functions:
+    - MIN(), MAX(), DATEDIFF()
+===============================================================================
+*/
+
+-- Get first and last order dates and duration in months
+SELECT 
+    MIN(order_date) AS first_order_date,
+    MAX(order_date) AS last_order_date,
+    DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
+FROM gold.fact_sales;
+
+-- Identify youngest and oldest customers by birthdate
+SELECT
+    MIN(birthdate) AS oldest_birthdate,
+    DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS oldest_age,
+    MAX(birthdate) AS youngest_birthdate,
+    DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS youngest_age
+FROM gold.dim_customers;
